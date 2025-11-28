28d2e8cf6dbc863c79534621ca34be9cd1633605
Intan-annisa <intanannisa2210@gmail.com>
2025-11-28 13:46:28 +0700

    update server.js

--- db.js
+++ db.js
@@ -3,9 +3,11 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = {
-  query: (text, params) => pool.query(text, params)
+  query: (text, params) => pool.query(text, params),
 };
